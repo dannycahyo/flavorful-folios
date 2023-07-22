4911889ac31ff211d01d81bf7836af1cdc61c4d6
dannycahyo <dannydwicahyono@gmail.com>
2023-07-22 21:10:42 +0700

    feat: Adjust CateringService component responsiveness

--- src/components/home/CateringService/CateringService.tsx
+++ src/components/home/CateringService/CateringService.tsx
@@ -1,19 +1,19 @@
 const CateringService = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center py-4 md:py-12">
       <div className="text-center w-4/5">
-        <h2 className="text-sm lg:text-xl xl:text-2xl font-bold mb-4">
+        <h2 className="text-sm lg:text-xl xl:text-2xl font-bold mb-4 text-gray-500">
           Catering services in New York
         </h2>
-        <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-bold ">
+        <h1 className="text-2xl sm:text-2xl lg:text-4xl xl:text-5xl font-bold text-blue-800">
           We specialize in corporate and private events
         </h1>
-        <p className="mt-4 text-sm lg:text-xl xl:text-2xl">
+        <p className="mt-4 text-sm md:text-md lg:text-lg xl:text-xl">
           With 20 years of experience, we promise you the freshest, local
           ingredients, hand-crafted cooking sprinkled with our unique whimsical
           elegance and exceptional service.
         </p>
-        <div className="w-full mt-4 border border-gray-300" />
+        <div className="w-full mt-4 border-2 border-gray-300" />
       </div>
     </div>
   );
