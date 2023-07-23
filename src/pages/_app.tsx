2eb6b493a6cb6b12964ddbf0473073e33450cda2
dannycahyo <dannydwicahyono@gmail.com>
2023-07-23 11:36:54 +0700

    fix: Fix httpEquiv wrong property

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Head>
-        <meta http-equiv="Content-Language" content="en" />
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Danny D Cahyono" />
         <meta name="robots" content="all" />
